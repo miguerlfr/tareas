936d79406663685b5ab107097207c4e00ec48ebd
miguerlfr <luismiguelvargasjaimes@gmail.com>
2024-02-14 21:56:34 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,178 @@
+<script setup>
+import { ref } from 'vue';
+
+let a = ref()
+let actividad = ref("")
+let fecha = ref("")
+let arr = ref([])
+function check() {
+  let prioridad = a.value === true ? 'Alta' : 'Baja';
+  arr.value.push({
+  actividad: actividad.value,
+  prioridad,
+  fecha: fecha.value
+})
+  actividad.value = ''
+  fecha.value = ''
+}
+
+function ordenar() {
+  arr.value.sort((z, b)=> z.prioridad.localeCompare(b.prioridad))
+}
+
+function eliminar(i) {
+  arr.value.splice(i, 1)
+}
+
+</script>
+
+<template>
+  <section class="papa">
+
+    <section class="hijo">
+
+      <input class="actividad" type="text" placeholder="     ✍🏼  Escribe tu asunto  ✍🏼" v-model="actividad">
+      <input class="fecha" type="date" v-model="fecha">
+      <button @click="check()">+</button>
+      <input type="checkbox" class="check" v-model="a">
+      <button @click="ordenar()">Ordenar</button>
+
+    </section>
+
+    <section class="hijoD">
+      <table>
+        <thead class="a">
+          <tr class="tHead">
+            <th>Actividad</th>
+            <th>Prioridad</th>
+            <th>Fecha</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="(item, i) in arr" :key="i">
+            <td>{{ item.actividad }}</td>
+            <td>{{ item.prioridad }}</td>
+            <td>{{ item.fecha }}</td>
+            <td>
+              <button class="eliminar" @click="eliminar(i)">❌</button>
+            </td>
+          </tr>
+        </tbody>
+
+      </table>
+
+
+    </section>
+
+  </section>
+</template>
+
+<style scoped>
+* {
+  color: white;
+}
+
+.actividad {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  width: 50rem;
+  height: 42px;
+  margin-top: -200px;
+  margin-left: -113px;
+  padding-left: 20px;
+}
+
+.fecha {
+  position: absolute;
+  margin-left: -90px;
+  transform: translate(-50%);
+  margin-top: -140px;
+  height: 40px;
+  width: 200px;
+  padding-left: 20px;
+}
+
+.papa {
+  background-color: white;
+  padding: 250px;
+  width: 650px;
+  height: 150px;
+}
+
+.hijo {
+  display: flex;
+  gap: 20px;
+}
+
+.hijoD {
+  padding-top: 10%;
+}
+
+/* Estilo del checkbox */
+.check {
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 40px;
+  height: 40px;
+  /* Ajuste del alto para que sea cuadrado */
+  border: 4px solid black;
+  /* Borde verde */
+  background-color: #ffffff;
+  /* Fondo blanco */
+  border-radius: 4px;
+  cursor: pointer;
+  position: relative;
+}
+
+/* Estilo del chulo (tick) cuando está marcado */
+.check:checked {
+  border: 4px solid black;
+  /* Borde verde */
+  background-color: rgb(0, 132, 255);
+  /* Fondo verde cuando está marcado */
+  border-color: rgb(0, 132, 255);
+  /* Borde verde cuando está marcado */
+}
+
+/* Estilo del chulo (tick) dentro del checkbox */
+.check:checked::before {
+  content: '\2713';
+  /* Código unicode para el símbolo de chulo */
+  display: block;
+  width: 100%;
+  height: 100%;
+  font-size: 25px;
+  /* Ajuste del tamaño del chulo para que sea más grande */
+  font-weight: bold;
+  /* Ajuste del peso del chulo */
+  color: #ffffff;
+  /* Color blanco del chulo */
+  text-align: center;
+  line-height: 35px;
+  /* Ajuste del line-height para centrar verticalmente el chulo */
+  position: absolute;
+  /* Posiciona el chulo absolutamente dentro del checkbox */
+  top: 0;
+  left: 0;
+}
+
+table {
+  border-collapse: collapse;
+  width: 60rem;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+}
+
+.tHead {
+  background-color: rgb(0, 132, 255);
+}
+
+.eliminar {
+  background-color: transparent;
+}
+</style>
